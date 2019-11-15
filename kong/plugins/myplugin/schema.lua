379927a324814255eebe9f5fa9a19c79d5b1bfa1
wuxin <wuxin.ext@gmail.com>
2019-11-15 16:18:52 +0800

    schema

--- kong/plugins/myplugin/schema.lua
+++ kong/plugins/myplugin/schema.lua
@@ -3,4 +3,20 @@
 --- Created by wuxin.
 --- DateTime: 2019-11-14 18:13
 ---
-return {}
+local typedefs = require "kong.db.schema.typedefs"
+
+
+return {
+    name = "myplugin",
+    fields = {
+        { consumer = typedefs.no_consumer },
+        { run_on = typedefs.run_on_first },
+        { protocols = typedefs.protocols_http },
+        { config = {
+            type = "record",
+            fields = {
+                { myconf = { type = "string", uuid = true, legacy = true }, },
+
+            }, }, },
+    },
+}
